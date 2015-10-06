8b79e3572ef7e6ff09cdab702b718253113398fb
Oscar <ochng6654@gmail.com>
2015-10-05 20:03:02 -0700

    Lesson 25

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry.rb"
+require "csv"
 class AddressBook
   attr_accessor :entries
 
@@ -16,7 +17,7 @@ class AddressBook
       @entries.delete(delete1)
   end
 
-   def add_entry(name, phone, email)
+  def add_entry(name, phone, email)
      index = 0
      @entries.each do |entry|
        if name < entry.name
@@ -25,5 +26,15 @@ class AddressBook
        index += 1
      end
      @entries.insert(index, Entry.new(name, phone, email))
+   end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+ # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
    end
 end
