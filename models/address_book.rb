e98175d097b53f93cff96a9c3c271c499472becf
Oscar <ochng6654@gmail.com>
2015-10-08 19:46:33 -0700

    Lesson 26

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,6 +17,37 @@ class AddressBook
       @entries.delete(delete1)
   end
 
+   # Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+     upper = entries.length - 1
+
+ # #2
+     while lower <= upper
+ # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+ # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+    return nil
+  end
+
+  def iterative_search(name)
+    @entries.each do|entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil
+  end
   def add_entry(name, phone, email)
      index = 0
      @entries.each do |entry|
